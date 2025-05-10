0929bf0ff0bcc7814f34ad52ec65bbf8999c470d
Kirubel Girma <Kirub1896@gmail.com>
2025-05-10 11:33:40 +0300

    version 1 complete

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -5,7 +5,7 @@ import { icons } from "@/constants/icons";
 const Profile = () => {
   return (
     <SafeAreaView className="bg-primary flex-1 px-10">
-      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+      <View className="flex flex-1 flex-col gap-5">
         <Image source={icons.person} className="size-10" tintColor="#fff" />
         <Text className="text-gray-500 text-base">Profile</Text>
       </View>
